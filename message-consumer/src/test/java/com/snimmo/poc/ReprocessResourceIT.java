7d93f61369666dc8388a9a57864d9760fa36ff5c
Stephen Nimmo <stephennimmo@gmail.com>
2022-06-29 11:10:48 -0500

    Project init

---
+++ message-consumer/src/test/java/com/snimmo/poc/ReprocessResourceIT.java
@@ -0,0 +1,9 @@
+package com.snimmo.poc;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ReprocessResourceIT extends ReprocessResourceTest {
+
+    // Execute the same tests but in native mode.
+}
